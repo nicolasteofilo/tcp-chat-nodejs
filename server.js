144d94e3352cb41485e3ffab3b232210dca61571
Nicolas Teófilo <nicolasteofilodecastro@gmail.com>
2025-12-23 10:11:26 -0300

    feat: broadcast messages to multiple TCP clients

--- server.js
+++ server.js
@@ -1,25 +1,43 @@
 const net = require("net")
 const PORT = 5000;
 
+// storage all connected clients in server
+const clients = new Set();
+
+function broadcast(msg, exceptSocket = null) {
+  for (const client of clients) {
+    if (client !== exceptSocket && !client.destroyed) {
+      client.write(msg)
+    }
+  }
+}
+
 const server = net.createServer((socket) => {
+  clients.add(socket);
   console.log("Client connected:", socket.remoteAddress, socket.remotePort, socket.remoteFamily);
 
-  socket.write("Welcome to the TCP server\n")
-  socket.write("Type some text:")
+  socket.write("Welcome to the TCP server\n");
+  socket.write("Open another terminal and connect with other client!\n");
+
+  broadcast("Someone enter in chat!\n", socket);
 
   socket.on("data", (chunck) => {
     // chunck is a Buffer (bytes)
     const text = chunck.toString("utf8");
-    console.log("Recevied from the client: ", JSON.stringify(text));
-    socket.write("Eco: " + text)
+
+    // send message to the orther client
+    broadcast(text, socket);
   })
 
   socket.on("end", () => {
-    console.log("Cliend desconnected")
+    clients.delete(socket);
+    console.log("Cliend desconnected!\n");
+    broadcast("Someone exit from the chat!\n")
   })
 
   socket.on("error", () => {
-    console.log("Error on socket: ", err.message)
+    clients.delete(socket);
+    console.log("Error on socket: ", err.message);
   })
 })
 
