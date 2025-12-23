9a9c6db787bb2a5d7a880b45d5e8bf8ea538f046
Nicolas Teófilo <nicolasteofilodecastro@gmail.com>
2025-12-23 09:57:08 -0300

    feat: echo back messages received over TCP

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const net = require("net")
 const PORT = 5000;
 
-const dgram = require("node:dgram")
-
-
 const server = net.createServer((socket) => {
   console.log("Client connected:", socket.remoteAddress, socket.remotePort, socket.remoteFamily);
 
   socket.write("Welcome to the TCP server\n")
+  socket.write("Type some text:")
+
+  socket.on("data", (chunck) => {
+    // chunck is a Buffer (bytes)
+    const text = chunck.toString("utf8");
+    console.log("Recevied from the client: ", JSON.stringify(text));
+    socket.write("Eco: " + text)
+  })
 
   socket.on("end", () => {
     console.log("Cliend desconnected")
